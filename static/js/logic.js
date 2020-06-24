75fda476922c258b1fa8709412f56b14057d9867
DigBaseball <misha.berkowitz@gmail.com>
2020-06-23 21:24:54 -0700

    Final build. Ready for grading.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,20 +1,20 @@
 // Create our map, giving it the streetmap and earthquakes layers to display on load
 var myMap = L.map("map", {
-    center: [37.38, -122.08],
-    zoom: 8
+    center: [0, 0],
+    zoom: 3
     //layers: [streetmap, earthquakes]
 });
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
       maxZoom: 18,
-      id: "mapbox/streets-v11",
+      id: "mapbox/light-v10",
       accessToken: "pk.eyJ1IjoiZGlnYmFzZWJhbGwiLCJhIjoiY2tic2NldHFsMDA4MDJxb2U2YnFhaXltaiJ9.1KdKHueX9Sc8DGAm-v9uhQ"
     }).addTo(myMap);
 
 
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
@@ -36,99 +36,66 @@ d3.json(queryUrl, function(data) {
 
     // Determine the color of the circle
     if (mag < 1) {
-        color = "YellowGreen";
+        color = "#B7F34D";
     }
     else if (mag < 2) {
-        color = "Yellow";
+        color = "#E1F34D";
     }
     else if (mag < 3) {
-        color = "Gold";
+        color = "#F3DC4D";
     }
     else if (mag < 4) {
-        color = "GoldenRod";
+        color = "#F3BA4C";
     }
     else if (mag < 5) {
-        color = "DarkGoldenRod";
+        color = "#F1A76A";
     }
     else {
-        color = "Red";
+        color = "#F06A6A";
     };
   
     // Add circles to map
-    L.circle(location, {
+    L.circleMarker(location, {
       fillOpacity: 0.75,
-      color: color,
+      color: "black",
       fillColor: color,
       // Adjust radius
-      radius: mag * 1500
+      radius: mag * 5,
+      weight: 1
     }).bindPopup("<h2>" + mag + " magnitude "+ eventType + "</h2><h3>" + eventPlace +
     "</h3><hr><p>" + new Date(eventTime) + "</p>").addTo(myMap);
   };
 
 });
 
+function getColor(m) {
+    return m > 5 ? '#F06A6A' :
+           m > 4 ? '#F1A76A' :
+           m > 3 ? '#F3BA4C' :
+           m > 2 ? '#F3DC4D' :
+           m > 1 ? '#E1F34D' :
+                    '#B7F34D';
+};
 
+// Create a legend
+var legend = L.control({position: 'bottomright'});
 
-/*
+// Populate the legend
+legend.onAdd = function (map) {
 
-function createFeatures(earthquakeData) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1, 2, 3, 4, 5],
+        labels = [];
 
-    // Define a function we want to run once for each feature in the features array
-    function onEachFeature(feature, layer) {
-        
-        // Create a variable for storing the magnitude
-        var mag = feature.properties.mag;
-        
-        // Give each feature a popup describing the magnitude, type, place and time of the incident
-        layer.bindPopup("<h2>" + mag + " magnitude "+ feature.properties.type + "</h2><h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-
-        
-        // Initialize a variable for storing the color of the marker
-        var color = "";
-
-        // Determine the color of the marker
-        if (mag < 1) {
-            color = "YellowGreen";
-        }
-        else if (mag < 2) {
-            color = "Yellow";
-        }
-        else if (mag < 3) {
-            color = "Gold";
-        }
-        else if (mag < 4) {
-            color = "GoldenRod";
-        }
-        else if (mag < 5) {
-            color = "DarkGoldenRod";
-        }
-        else {
-            color = "Red";
-        };
-
-        // console.log(`The color is ${color} and the mag is ${mag} and the type is ${feature.properties.type}`);
-        
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
     }
-  
-    // Create a GeoJSON layer containing the features array on the earthquakeData object
-    // Run the onEachFeature function once for each piece of data in the array
-    var earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature
-    });
-    
-    // Define streetmap layer
-    var streetmap = 
-    
-    // Define a baseMaps object to hold our base layer
-    var baseMaps = {
-        "Street Map": streetmap
-    };
-    
-    // Create overlay object to hold our overlay layer
-    var overlayMaps = {
-        Earthquakes: earthquakes
-    };
 
+    return div;
+};
 
-};*/
+// Add the legend to the map
+legend.addTo(myMap);
